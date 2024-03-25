c86ad8199edde29b00742aa3bb009eb0b49e5ad8
rizwanali2202 <164035652+rizwanali2202@users.noreply.github.com>
2024-03-25 16:48:28 +0500

    Name case
    we create the program in Name cases. lower cases, upper cases and title cases

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let userName = "rizwan ali";
+console.log("Lower Case =", userName.toLowerCase());
+console.log("Upper Case =", userName.toUpperCase());
+console.log("Title Case =", userName.charAt(0).toUpperCase() + userName.slice(1));
---
+++ app.ts
@@ -0,0 +1,4 @@
+let userName : string = "rizwan ali";
+console.log("Lower Case =", userName.toLowerCase());
+console.log("Upper Case =", userName.toUpperCase());
+console.log("Title Case =", userName.charAt(0).toUpperCase()+ userName.slice(1));
